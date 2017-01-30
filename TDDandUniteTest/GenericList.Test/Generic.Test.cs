cdee528fca95c0eb5f097a1f69dc382db3569faf
ondercalbay <ondercalbay.com>
2017-01-30 15:35:17 +0300

    c

---
+++ TDDandUniteTest/GenericList.Test/Generic.Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenericList.Test;
+
+namespace GenericList.Test
+{
+    [TestClass]
+    public class Generic
+    {
+        GenericList<string> list { get; set; }
+
+        [TestInitialize]
+        public void InitializationTest()
+        {
+            list = new GenericList<string>();
+            Assert.AreEqual(list.Count, 0, "Liste ilk yaratıldığında listenin eleman sayısı sıfır olmalıdır.");
+        }
+
+        [TestMethod]
+        public void AddItemTest()
+        {         
+            list.AddItem("Yeni bir liste elemanı");
+            Assert.AreEqual<int>(list.Count, 1, "Listenin eleman sayısı yeni bir eleman eklendiğinde bir artmalıdır.");
+        }
+
+        [TestMethod]
+        public void RemoveItemIndexTest()
+        {
+            int beforeCount = list.Count;
+            list.RemoveItem(0);
+            Assert.AreEqual<int>(beforeCount - list.Count, 1, "Listenin eleman sayısı yeni bir eleman eklendiğinde bir azalmalıdır.");
+        }
+    }
+}
---
+++ TDDandUniteTest/GenericList.Test/GenericList.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+namespace GenericList.Test
+{
+    class GenericList<T>
+    {
+        private T[] m_InnerItems = new T[0];
+
+        private int m_Count = 0;
+        public int Count
+        {
+            get { return m_Count; }
+        }
+
+        public void AddItem(T item)
+        {
+            T[] newItemsArray = new T[m_Count + 1];
+            Array.Copy(m_InnerItems, newItemsArray, m_InnerItems.Length);
+            newItemsArray[m_Count] = item;
+            this.m_InnerItems = newItemsArray;
+            m_Count++;
+        }
+
+        internal void RemoveItem(int index)
+        {
+            T[] newItemsArray = new T[m_Count - 1];
+
+            for (int i = 0; i < m_InnerItems.Length; i++)
+            {
+                if (index >= m_InnerItems.Length)
+                {
+                    newItemsArray[i] = m_InnerItems[i];
+                }
+                else
+                {
+                    newItemsArray[i - 1] = m_InnerItems[i];
+                }
+
+            }
+            this.m_InnerItems = newItemsArray;
+            m_Count--;
+        }
+    }
+}
